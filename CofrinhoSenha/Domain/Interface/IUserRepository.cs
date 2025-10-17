f5dfbe8482f4ea8598367a9f1011a858ae2f8e75
Arthur <nashcassano8@gmail.com>
2025-10-17 13:42:42 -0300

    adicionando interface e repository da entidade user

---
+++ CofrinhoSenha/Domain/Interface/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using CofrinhoSenha.Domain.Entity;
+
+namespace CofrinhoSenha.Domain.Interface;
+
+public interface IUserRepository
+{
+    Task<User> GetUserById(int id);
+
+    Task<bool> Exists(int id);
+    Task<bool> SaveUser(User user);
+    
+    Task<bool> UpdateUser(User user);
+    
+    Task<bool> DeleteUser(User user);
+}
---
+++ CofrinhoSenha/Infrastructure/Data/Repository/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using CofrinhoSenha.Domain.Entity;
+using CofrinhoSenha.Domain.Interface;
+using CofrinhoSenha.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CofrinhoSenha.Infrastructure.Data.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<User> GetUserById(int id)
+    {
+        return await _context.User.Where(u => u.Id == id).FirstOrDefaultAsync();
+    }
+
+
+    public async Task<bool> Exists(int id)
+    {
+        return await _context.User.AnyAsync(u => u.Id == id);
+    }
+
+    public async Task<bool> SaveUser(User user)
+    {
+        _context.User.Add(user);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> UpdateUser(User user)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> DeleteUser(User user)
+    {
+        _context.Remove(user);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
