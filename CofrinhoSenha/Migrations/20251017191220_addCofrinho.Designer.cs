7cd08110bc766cb82eb50dbefac3d36320555bbe
Arthur <nashcassano8@gmail.com>
2025-10-17 16:15:39 -0300

    adicionando migrations do cofrinho

--- CofrinhoSenha/Migrations/20251017191220_addCofrinho.Designer.cs
+++ CofrinhoSenha/Migrations/20251017191220_addCofrinho.Designer.cs
@@ -2,6 +2,7 @@
 using CofrinhoSenha.Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CofrinhoSenha.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251017191220_addCofrinho")]
+    partial class addCofrinho
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
