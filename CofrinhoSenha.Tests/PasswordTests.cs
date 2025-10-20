1684a55175c71277f4b97e02c30631f033172588
Arthur <nashcassano8@gmail.com>
2025-10-20 17:39:57 -0300

    criando os testes para o GeneratePassword do Service. Foram testadas as seguinte condicoes: se retorna senhas diferentes, se a senha eh do tamanho padrao, se a senha eh do tamanho imposto pelo usuario e se a senha foi maior que o padrao limitado

--- CofrinhoSenha.Tests/PasswordTests.cs
+++ CofrinhoSenha.Tests/PasswordTests.cs
@@ -1,4 +1,5 @@
 ﻿using CofrinhoSenha.Data.Context;
+using CofrinhoSenha.Entity;
 using CofrinhoSenha.Service;
 using Microsoft.EntityFrameworkCore;
 
@@ -71,5 +72,82 @@ public class PasswordTests
         var resultado = service.VerifyPassword(hashInvalido, "santosfc");
         
         Assert.False(resultado);
+    }
+
+    [Fact]
+    public void DeveMeRetornarUmaSenhaDoTamanhoPadrao40()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "Testdb")
+            .Options;
+
+        var context = new AppDbContext(options);
+        var service = new PasswordService(context);
+        
+        var request = new GeneratePasswordRequest();
+        var cofrinho = new Cofrinho();
+
+        var resultado = service.GenerateStrongPassword(request, cofrinho.Id);
+        
+        Assert.Equal(40, resultado.Nome.Length);
+    }
+    
+    [Fact]
+    public void DeveMeRetornarUmaSenhaDeTamanhoEscolhidoPeloUsuario()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "Testdb")
+            .Options;
+
+        var context = new AppDbContext(options);
+        var service = new PasswordService(context);
+        
+        var request = new GeneratePasswordRequest();
+        var cofrinho = new Cofrinho();
+        request.Length = 10;
+
+        var resultado = service.GenerateStrongPassword(request, cofrinho.Id);
+        
+        Assert.Equal(10, resultado.Nome.Length);
+    }
+    
+    [Fact]
+    public void DeveFalharSeASenhaForMaiorQue40()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "Testdb")
+            .Options;
+        
+        var context = new AppDbContext(options);
+        var service = new PasswordService(context);
+        
+        var request = new GeneratePasswordRequest();
+        var cofrinho = new Cofrinho();
+        request.Length = 41;
+        
+        var resultado = service.GenerateStrongPassword(request, cofrinho.Id);
+        
+        Assert.NotNull(resultado);
+        Assert.NotNull(resultado.Nome);
+        Assert.False(resultado.Nome.Length <= 40);
+    }
+
+    [Fact]
+    public void DeveMeRetornarSenhasDiferentes()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "Testdb")
+            .Options;
+
+        var context = new AppDbContext(options);
+        var service = new PasswordService(context);
+        
+        var request = new GeneratePasswordRequest();
+        var cofrinho = new Cofrinho();
+
+        var senha1 = service.GenerateStrongPassword(request, cofrinho.Id);
+        var senha2 = service.GenerateStrongPassword(request, cofrinho.Id);
+        
+        Assert.NotEqual(senha1, senha2);
     }
 }
--- CofrinhoSenha/Entity/GeneratePasswordRequest.cs
+++ CofrinhoSenha/Entity/GeneratePasswordRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace CofrinhoSenha.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CofrinhoSenha.Entity;
 
 public class GeneratePasswordRequest
 {
+    
+    [Range(1, 40, ErrorMessage = "A senha nao deve ser maior do que 40")]
     public int Length { get; set; } = 40;
     
     public int CofrinhoId { get; set; }
