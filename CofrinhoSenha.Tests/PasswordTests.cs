2e7df08db27957c7dd794c18c1735fbcaafbd153
Arthur <nashcassano8@gmail.com>
2025-10-20 16:23:43 -0300

    adicionando o teste de verificar se a minha regra de negocio de verificar senha esta correto

---
+++ CofrinhoSenha.Tests/PasswordTests.cs
@@ -0,0 +1,24 @@
+﻿using CofrinhoSenha.Data.Context;
+using CofrinhoSenha.Service;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CofrinhoSenha.Tests;
+
+public class PasswordTests
+{
+    [Fact]
+    public void VerificaSeSenhaEstaCorretaDeveRetornarTrue()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "Testdb")
+            .Options;
+        
+        var context = new AppDbContext(options);
+        var service = new PasswordService(context);
+
+        var resultado = service.VerifyPassword("santosfc", "santosfc");
+        
+        Assert.True(resultado);
+    }
+}
