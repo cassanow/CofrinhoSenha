5cdf6e5d863a184893d4040f619bef802fb9e37b
Arthur <nashcassano8@gmail.com>
2025-10-22 18:00:32 -0300

    fazendo a DI na classe de testes de integracao

--- CofrinhoSenha.Tests/PasswordIntegrationTests.cs
+++ CofrinhoSenha.Tests/PasswordIntegrationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Security.Claims;
@@ -6,6 +7,7 @@ using System.Text;
 using CofrinhoSenha.Data.Context;
 using CofrinhoSenha.Entity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -13,9 +15,21 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace CofrinhoSenha.Tests;
 
-public class PasswordIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+public class PasswordIntegrationTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
-   private readonly HttpClient _client;
-   private readonly WebApplicationFactory<Program> _factory;
+    private readonly CustomWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _client;
 
+    public PasswordIntegrationTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false,
+            BaseAddress = new Uri("https://localhost"),
+            HandleCookies = false,
+            MaxAutomaticRedirections = 7
+        });
+    }
+    
 }
