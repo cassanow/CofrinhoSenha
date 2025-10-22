921d8adacb46bf174b43e4909a1e49745bd59770
Arthur <nashcassano8@gmail.com>
2025-10-22 16:29:17 -0300

    apagando os testes feitos anteriormente e recomecando

---
+++ CofrinhoSenha.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using CofrinhoSenha.Data.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace CofrinhoSenha.Tests;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType ==
+                                                                    typeof(IDbContextOptionsConfiguration<
+                                                                        AppDbContext>));
+
+
+        });
+    }
+}
